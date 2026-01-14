7502f1bd1671a2b8f6d29316ecf65de57cb05b7d
RandomUserIK <kovac.ivan97@icloud.com>
2026-01-14 22:17:42 +0100

    feat(chapter-10): add integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,15 +22,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.cloud:spring-cloud-function-context")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
 	implementation("io.github.oshai:kotlin-logging-jvm:${property("kotlin-logging-jvm.version")}")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("io.kotest:kotest-assertions-core-jvm:${property("kotest.version")}")
+	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
