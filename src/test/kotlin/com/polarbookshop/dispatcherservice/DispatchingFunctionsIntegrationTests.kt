bf26ae86b2fbdeb44675a06d5de4082ca6d6768a
RandomUserIK <kovac.ivan97@icloud.com>
2026-01-14 22:17:42 +0100

    feat(chapter-10): add integration tests

--- src/test/kotlin/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.kt
+++ src/test/kotlin/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.kt
@@ -1,5 +1,6 @@
 package com.polarbookshop.dispatcherservice
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.function.context.FunctionCatalog
@@ -9,9 +10,40 @@ import reactor.test.StepVerifier
 import java.util.function.Function
 
 @FunctionalSpringBootTest
+@Suppress("ReactiveStreamsUnusedPublisher")
 internal class DispatchingFunctionsIntegrationTests @Autowired constructor(
 	private val catalog: FunctionCatalog,
 ) {
+
+	@Test
+	fun pack() {
+		val pack = catalog.lookup<Function<OrderAcceptedMessage, Long>>(
+			Function::class.java,
+			"pack",
+		)
+		val orderId = 666L
+
+		pack.apply(
+			OrderAcceptedMessage(orderId)
+		) shouldBe orderId
+	}
+
+	@Test
+	fun label() {
+		val label = catalog.lookup<Function<Flux<Long>, Flux<OrderDispatchedMessage>>>(
+			Function::class.java,
+			"label",
+		)
+		val orderId = 666L
+		val orderFlux = Flux.just(orderId)
+
+		StepVerifier
+			.create(
+				label.apply(orderFlux)
+			)
+			.expectNextMatches { it.id == orderId }
+			.verifyComplete()
+	}
 
 	@Test
 	fun packAndLabelOrder() {
